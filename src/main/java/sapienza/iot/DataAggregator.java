5b78344661b8ba6bb4f7c16a917455421da36aef
Mohammed Sukhsarwala <43992747+momo1901@users.noreply.github.com>
2019-04-13 17:11:55 +0200

    Update DataAggregator.java

--- src/main/java/sapienza/iot/DataAggregator.java
+++ src/main/java/sapienza/iot/DataAggregator.java
@@ -2,10 +2,7 @@ package sapienza.iot;
 
 import com.google.gson.JsonObject;
 import org.apache.edgent.function.Supplier;
-import sapienza.iot.Interfaces.ReadDoubleSensor;
-import sapienza.iot.Interfaces.ReadFloatSensor;
-import sapienza.iot.Interfaces.ReadIntSensor;
-import sapienza.iot.Interfaces.ReadLocationSensor;
+import sapienza.iot.Interfaces.ReadDoubleDecibel;
 
 import java.time.LocalDateTime;
 
